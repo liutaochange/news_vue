2a620960867c874eac312de885d52e79024ebde4
liutaochange <liutaochange@163.com>
2018-03-30 20:53:03 +0800

    change style

--- src/main.js
+++ src/main.js
@@ -10,15 +10,16 @@ import store from './store'
 Vue.use(MuseUI)
 fastclick.attach(document.body)
 Vue.config.productionTip = false
-window.onload = function () {
-  getRem(640, 100)
-}
+getRem(640, 100)
 window.onresize = function () {
   getRem(640, 100)
 }
 function getRem (pwidth, prem) {
   var html = document.getElementsByTagName('html')[0]
   var oWidth = document.body.clientWidth || document.documentElement.clientWidth
+  if (oWidth >= 640) {
+    oWidth = 640
+  }
   html.style.fontSize = oWidth / pwidth * prem + 'px'
 }
 
